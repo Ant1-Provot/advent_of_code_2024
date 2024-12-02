b9d566d8836738a024d1b7eed68ea0685d38a349
Antoine Provot <antoine@sandboxquantum.com>
2024-12-02 10:22:51 +0100

    Day 2

--- d_1/main.ml
+++ d_1/main.ml
@@ -1,56 +1,44 @@
 let file = "reference.txt"
-let regex = Str.regexp "\\([0-9]+\\)   \\([0-9]+\\)" 
+let regex = Str.regexp "\\([0-9]+\\)   \\([0-9]+\\)"
 
-let make_lists ic = 
-  let rec aux lhs rhs = 
+let make_lists ic =
+  let rec aux lhs rhs =
     try
       let line = input_line ic in
       let _ = Str.string_match regex line 0 in
       let l = int_of_string (Str.matched_group 1 line) in
       let r = int_of_string (Str.matched_group 2 line) in
-      aux (l::lhs) (r::rhs)
-
-    with
-    | End_of_file -> 
-      (lhs, rhs)
+      aux (l :: lhs) (r :: rhs)
+    with End_of_file -> (lhs, rhs)
   in
   aux [] []
 
-
-let q_1 lhs rhs = 
-  let sort l = 
-    List.sort compare l
-  in
+let q_1 lhs rhs =
+  let sort l = List.sort compare l in
 
   let lhs = sort lhs in
   let rhs = sort rhs in
 
-  let diffs = List.map2 (fun a b ->     
-    abs(a-b) ) lhs rhs in
-  List.fold_left (+) 0 diffs
+  let diffs = List.map2 (fun a b -> abs (a - b)) lhs rhs in
+  List.fold_left ( + ) 0 diffs
 
 let q_2 lhs rhs =
   let count l v =
-    List.fold_left (fun acc x -> if x = v then acc +1 else acc) 0 l
+    List.fold_left (fun acc x -> if x = v then acc + 1 else acc) 0 l
   in
 
-  List.fold_left (fun acc el -> 
-    acc + ((count rhs el) * el) )
-    0 
-    lhs
-
+  List.fold_left (fun acc el -> acc + (count rhs el * el)) 0 lhs
 
 let () =
   (* Read file and display the first line *)
   let ic = open_in file in
-    let (lhs, rhs) = make_lists ic in 
-    close_in ic;
+  let lhs, rhs = make_lists ic in
+  close_in ic;
 
-    print_string "q1 = ";
-    print_int (q_1 lhs rhs);    
-    print_newline ();
+  print_string "q1 = ";
+  print_int (q_1 lhs rhs);
+  print_newline ();
 
-    print_string "q2 = ";
-    print_int (q_2 lhs rhs);    
-    print_newline ();
-
+  print_string "q2 = ";
+  print_int (q_2 lhs rhs);
+  print_newline ()
--- d_2/main.ml
+++ d_2/main.ml
@@ -1,1 +1,58 @@
-let () = print_endline "Hello, World!"
+let file = "reference.txt"
+let count_true = List.fold_left (fun acc x -> if x then acc + 1 else acc) 0
+
+let report_is_safe r =
+  let rec aux is_inc is_dec = function
+    | [] | [ _ ] -> is_inc || is_dec
+    | h :: g :: t ->
+        let is_inc = is_inc && g - h >= 1 && g - h <= 3 in
+        let is_dec = is_dec && h - g >= 1 && h - g <= 3 in
+        aux is_inc is_dec (g :: t)
+  in
+  aux true true r
+
+let make_lists ic =
+  let rec aux acc =
+    try
+      let line = input_line ic in
+      let l = line |> String.split_on_char ' ' |> List.map int_of_string in
+      aux (l :: acc)
+    with End_of_file -> acc
+  in
+  aux []
+
+let q_1 l = l |> List.map report_is_safe |> count_true
+
+let q_2 l =
+  let has_a_safe_perm report =
+    let without_i index report =
+      report |> List.filteri (fun i _ -> i <> index)
+    in
+
+    report
+    |> List.mapi (fun i _ -> report |> without_i i)
+    |> List.map report_is_safe
+    |> List.fold_left ( || ) false
+  in
+
+  let safe_reports = l |> List.filter report_is_safe |> List.length in
+  let unsafe_reports = l |> List.filter (fun x -> not (report_is_safe x)) in
+
+  let safe_dampened_reports =
+    unsafe_reports |> List.map has_a_safe_perm |> count_true
+  in
+
+  safe_reports + safe_dampened_reports
+
+let () =
+  (* Read file and display the first line *)
+  let ic = open_in file in
+  let reports = make_lists ic in
+  close_in ic;
+
+  print_string "q1 = ";
+  print_int (q_1 reports);
+  print_newline ();
+  print_string "q2 = ";
+  print_int (q_2 reports);
+  print_newline ()
